58c078c15205e286e9b194c3955d55c26d012b46
rmadhavarao1101 <75805576+rmadhavarao1101@users.noreply.github.com>
2026-01-12 17:27:24 -0400

    Replace sensitive data with placeholder values
    Updated provider configuration with placeholder values.

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "oci" {
-#  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaqkfafm7ykyzmszzdoe3hvimyx5dszcvg7fiju4tjrkkqt7crdn7a"
-#  user_ocid = "ocid1.user.oc1..aaaaaaaa44yre4fqhotsfvchi6r35oa5ielymjlpbrim5v7lkjonli7wriqa"
+#  tenancy_ocid = "mytenancyid"
+#  user_ocid = "myocid"
 #  private_key_path = "/Users/Rajesh.Madhavarao/Documents/terraform/myprojcode/rajesh_tf_mi_priv.pem"
-#  fingerprint = "8f:51:56:27:6b:4a:14:c3:de:cd:89:61:62:ad:8c:cb"
+#  fingerprint = "mykey"
 #  region = "ca-toronto-1"
 
   tenancy_ocid = var.tenancy_ocid
